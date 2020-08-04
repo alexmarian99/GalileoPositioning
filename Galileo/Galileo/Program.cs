cfede429177755cec9afc9f4b8fac420f41c32be
Alexandru-Marian Buza <alex.marian@brolake.ro>
2020-08-04 20:47:46 +0300

    Begin

---
+++ Galileo/Galileo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Galileo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Galileo/Library/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Galileo
+{
+    public class Class1
+    {
+    }
+}
