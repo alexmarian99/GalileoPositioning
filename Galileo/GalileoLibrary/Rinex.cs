e61c4728113808ea4b536443544e7038c85d5da6
Alexandru-Marian Buza <alex.marian@brolake.ro>
2020-08-06 21:45:14 +0300

    Revert ""
    This reverts commit 189b95ff701d28e4ac29a58f09f80e8f15fc8d78.

--- Galileo/GalileoLibrary/Rinex.cs
+++ Galileo/GalileoLibrary/Rinex.cs
@@ -12,46 +12,29 @@ namespace Galileo
 {
     public class Rinex
     {
-        // Regex variable for removing multiple spaces
         private static RegexOptions options = RegexOptions.None;
         private static Regex regex = new Regex("[ ]{2,}", options);
+        public Classes.RinexObservation ObservationFile { get; internal set; }
 
-        /// <summary>
-        /// The structure of a <b>Rinex Observation</b> file
-        /// </summary>
-        public Classes.RinexObservation ObservationFile { get; internal set; } = new RinexObservation();
-
-        /// <summary>
-        /// The structure of a <b>Navigation Observation</b> file
-        /// </summary>
         public Classes.RinexNavigation NavigationFile { get; internal set; }
 
-        /// <summary>
-        /// Reads a Rinex observation or navigation file and uploads it to the current object.
-        /// </summary>
-        /// <param name="path">File path</param>
-        /// <returns>True if the operation was successful or false in case of an error</returns>
         public bool ReadFIle(string path)
         {
-            // Reads the file from the specified path as a text
+            // Citirea de fisier ca text
             string fisier = File.ReadAllText(path);
 
-            // Check if it is an observation or navigation file
+            // Se verifica daca este un fisier de navigatie sau de observatie
             if (!(fisier.Contains("OBSERVATION DATA") || fisier.Contains("NAV DATA")))
             {
                 return false;
             }
 
-            // Filling in data in case is an observation file
             if (fisier.Contains("OBSERVATION DATA"))
             {
-                // Only the header part is selected
+                ObservationFile = new Classes.RinexObservation();
                 string header = fisier.Split("END OF HEADER")[0];
-
-                // Separate each line from each other
                 string[] liniiHeader = header.Split('\n');
 
-                // Go through each line to load the data
                 foreach (string linie in liniiHeader)
                 {
                     if (linie.Contains("RINEX VERSION / TYPE"))
@@ -217,7 +200,6 @@ namespace Galileo
                     }
                 }
 
-                // The part after the header, the one containing the data, is loaded
                 string continut = fisier.Split("END OF HEADER")[1];
                 List<string> records = continut.Split("> ").ToList();
                 records.Remove(records.First());
@@ -261,7 +243,6 @@ namespace Galileo
 
             }
 
-            // Filling in data in case is an navigation file
             else if (fisier.Contains("NAV DATA"))
             {
                 NavigationFile = new Classes.RinexNavigation();
